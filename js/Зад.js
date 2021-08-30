4444378f8f7fc5eec92c7509b6d86d8673a2d0be
Dragon2517 <rabvasya1994@yandex.ru>
2021-08-31 00:21:09 +0300

    first commit

---
+++ js/Зад.js
@@ -0,0 +1,48 @@
+
+
+
+
+ X1= window.innerWidth
+ Y1 = window.innerHeight
+
+
+let x = 100;
+let y = 2000;
+
+if (X1 > Y1 * 1.78) {
+    x = X1;
+    y = x / 1.78;
+}
+else {
+    y = Y1;
+    x = y * 1.78;
+};
+
+
+
+window.onresize = doALoadOfStuff;
+
+
+function doALoadOfStuff() {
+var root = document.documentElement;
+
+ X1= window.innerWidth
+ Y1 = window.innerHeight
+
+
+   if (X1 > Y1 * 1.78) {
+    x = X1;
+    y = x / 1.78;
+}
+    else {
+    y = Y1;
+    x = y * 1.78;
+};
+
+root.style.backgroundSize = x + "px"+" " + y +"px";
+}
+var root = document.documentElement;
+
+root.style.backgroundSize = x + "px"+" " + y +"px";
+
+
